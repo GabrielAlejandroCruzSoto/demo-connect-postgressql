dff861e24538dc8a8ea1a6e733f65396acc0ecbf
Gabriel Alejandro Cruz Soto <gabrielcrsoto@outlook.com>
2021-11-03 00:21:47 -0300

    proyect init

---
+++ src/main/kotlin/cl/gabrielcrsoto/service/web/democonnectpostgressql/DemoConnectPostgressqlApplication.kt
@@ -0,0 +1,11 @@
+package cl.gabrielcrsoto.service.web.democonnectpostgressql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoConnectPostgressqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoConnectPostgressqlApplication>(*args)
+}
---
+++ src/test/kotlin/cl/gabrielcrsoto/service/web/democonnectpostgressql/DemoConnectPostgressqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package cl.gabrielcrsoto.service.web.democonnectpostgressql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoConnectPostgressqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
